0f1984781fae8bc2d0f53b55ca3016b33d512cdb
Kassim <kassimhuggins@macbook-pro.attlocal.net>
2021-04-19 22:34:50 -0700

    deploy

---
+++ client/build/precache-manifest.c17ce222b0f820cd3d4e59c5ba7a6ac8.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "efef995f5992d4a6abd45c6edb7da162",
+    "url": "/index.html"
+  },
+  {
+    "revision": "fd51a1374083b5db8067",
+    "url": "/static/css/main.88131917.chunk.css"
+  },
+  {
+    "revision": "2b46ec7a6e1e2a9ebf48",
+    "url": "/static/js/2.736fc311.chunk.js"
+  },
+  {
+    "revision": "aa4100970f46d7e0ec2af84de3f2740b",
+    "url": "/static/js/2.736fc311.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "fd51a1374083b5db8067",
+    "url": "/static/js/main.09b726a9.chunk.js"
+  },
+  {
+    "revision": "02751e160f988a50d44f",
+    "url": "/static/js/runtime-main.8a258a78.js"
+  },
+  {
+    "revision": "ab4978553bd68ca15e503a0da7fa604d",
+    "url": "/static/media/spinner.ab497855.gif"
+  }
+]);
